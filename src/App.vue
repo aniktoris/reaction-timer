832c09152d3e73574d96a6927befdbbeb336354a
oleno <es.moname@gmail.com>
2024-05-14 20:07:22 +0200

    add timer and results

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results :score="score" :showResults="showResults"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data(){
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
---
+++ src/components/Results.vue
@@ -0,0 +1,9 @@
+<template>
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+  props: ['score', 'showResults']
+}
+</script>
